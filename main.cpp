9afe14fbb0ddbb3811622a26cdda5cf2dafc1740
gulperii <ezgi.gulperi@gmail.com>
2020-01-02 01:23:52 +0300

    works but problems

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <vector>
 #include <map>
+#include <sstream>
+
 
 using namespace std;
 
@@ -20,19 +22,18 @@ struct customer_thread_data {
 };
 int electricity, water, gas, telecommunications, cableTv = 0;
 ofstream outfile;
-string input_log;
 int NUM_CUSTOMERS;
 int NUM_ATMS = 10;
 map<string, int> bill_map = {{"electricity",        0},
                              {"water",              1},
                              {"gas",                2},
-                             {"telecommunications", 3},
-                             {"cableTv",            4}};
+                             {"telecommunication", 3},
+                             {"cableTV",            4}};
 map<int, string> rev_bill_map = {{0, "electricity"},
                                  {1, "water"},
                                  {2, "gas"},
-                                 {3, "telecommunications"},
-                                 {4, "cableTv"}};
+                                 {3, "telecommunication"},
+                                 {4, "cableTV"}};
 
 vector<vector<int>> request = {{-1, -1, -1},
                                {-1, -1, -1},
@@ -78,22 +79,32 @@ int pay(int customer_id, int bill_id, int amount) {
             pthread_mutex_lock(&m_electricity);
             electricity += amount;
             pthread_mutex_unlock(&m_electricity);
-        case 2:
-            pthread_mutex_lock(&m_gas);
-            gas += amount;
-            pthread_mutex_unlock(&m_gas);
+            break;
+
         case 1:
             pthread_mutex_lock(&m_water);
             water += amount;
             pthread_mutex_unlock(&m_water);
+            break;
+        case 2:
+            pthread_mutex_lock(&m_gas);
+            gas += amount;
+            pthread_mutex_unlock(&m_gas);
+            break;
+
+
         case 3:
             pthread_mutex_lock(&m_telecommunications);
             telecommunications += amount;
             pthread_mutex_unlock(&m_telecommunications);
+            break;
+
         case 4:
             pthread_mutex_lock(&m_cableTv);
             cableTv += amount;
             pthread_mutex_unlock(&m_cableTv);
+            break;
+
     }
     return 1;
 }
@@ -102,40 +113,33 @@ int pay(int customer_id, int bill_id, int amount) {
 void *runner(void *threadarg) {
     struct atm_thread_data *my_data;
     my_data = (struct atm_thread_data *) threadarg;
-    while (request[my_data->atm_Id][0] == -1) {}
-    if (pay(request[my_data->atm_Id][0], request[my_data->atm_Id][1], request[my_data->atm_Id][2])) {
-        request[my_data->atm_Id] = {-1, -1, -1};
-        pthread_mutex_lock(&m_write);
-        string line =
-                "Customer" + to_string(request[my_data->atm_Id][0]) + "," + to_string(request[my_data->atm_Id][2]) +
-                "TL," + rev_bill_map[request[my_data->atm_Id][1]] + "\n";
-        outfile << line;
-        pthread_mutex_unlock(&m_write);
-    }
+    while (request[my_data->atm_Id - 1][0] == -1) {}
+    pay(request[my_data->atm_Id - 1][0], request[my_data->atm_Id - 1][1], request[my_data->atm_Id - 1][2]);
+    pthread_mutex_lock(&m_write);
+    string line =
+            "Customer" + to_string(request[my_data->atm_Id - 1][0]) + "," +
+            to_string(request[my_data->atm_Id - 1][2]) +
+            "TL," + rev_bill_map[request[my_data->atm_Id - 1][1]] + "\n";
+    outfile << line;
+    pthread_mutex_unlock(&m_write);
+    request[my_data->atm_Id - 1] = {-1, -1, -1};
+
 }
 
 
 void *request_payment(void *threadarg) {
     struct customer_thread_data *my_data;
     my_data = (struct customer_thread_data *) threadarg;
-
-    cout << "Sleep time : " << my_data->sleep_time;
-    sleep(my_data->sleep_time);
-
-    if (sleep) {
-        pthread_mutex_lock(my_data->ATM_mutex);
-        if (my_data->bill_type == "electricity") {
-            int bill = 0;
-        }
-        request[my_data->ATM_id] = {my_data->customer_id, my_data->bill_id, my_data->payment};
-        while (request[my_data->ATM_id][0] != -1) {}
-        pthread_mutex_unlock(my_data->ATM_mutex);
-    }
-
-    pthread_exit(NULL);
+    usleep(my_data->sleep_time);
+    pthread_mutex_lock(my_data->ATM_mutex);
+    request[my_data->ATM_id - 1] = {my_data->customer_id, my_data->bill_id, my_data->payment};
+   while (request[my_data->ATM_id - 1][0] != -1) {}
+    pthread_mutex_unlock(my_data->ATM_mutex);
+    cout << my_data->customer_id;
+    pthread_exit(0);
 }
 
-void create_atms(pthread_t *at, atm_thread_data *atd) {
+bool create_atms(pthread_t *at, atm_thread_data *atd) {
     int rc;
     for (int i = 0; i < NUM_ATMS; i++) {
         atd[i].atm_Id = i + 1;
@@ -145,77 +149,105 @@ void create_atms(pthread_t *at, atm_thread_data *atd) {
         cout << "Error:unable to create thread," << rc << endl;
         exit(-1);
     }
+    return true;
 }
 
 void create_customers(ifstream &file, pthread_t *ct, customer_thread_data *ctd) {
     int rc;
-
-    for (int i = 0; i < NUM_CUSTOMERS; i++) {
-        //Customer id 1 den başlıyor
+    string dummy;
+    int i = 0;
+    while (getline(file, dummy)) {
+        stringstream ss(dummy);
+        string sleep, atm_id, bill_t, amount;
+        getline(ss, sleep, ',');
+        getline(ss, atm_id, ',');
+        getline(ss, bill_t, ',');
+        getline(ss, amount, ',');
         ctd[i].customer_id = i + 1;
-        file >> ctd[i].sleep_time;
-        // ATM id bir eksili
-        file >> ctd[i].ATM_id;
-        file >> ctd[i].bill_type;
+        ctd[i].sleep_time = stoi(sleep);
+        ctd[i].ATM_id = stoi(atm_id);
+        ctd[i].bill_type = bill_t;
         ctd[i].bill_id = bill_map[ctd[i].bill_type];
-        file >> ctd[i].payment;
+        ctd[i].payment = stoi(amount);
         switch (ctd[i].ATM_id) {
             case 1:
                 ctd[i].ATM_mutex = &atm1;
+                break;
             case 2:
                 ctd[i].ATM_mutex = &atm2;
+                break;
 
             case 3:
                 ctd[i].ATM_mutex = &atm3;
+                break;
             case 4:
                 ctd[i].ATM_mutex = &atm4;
+                break;
 
             case 5:
                 ctd[i].ATM_mutex = &atm5;
+                break;
 
             case 6:
                 ctd[i].ATM_mutex = &atm6;
+                break;
             case 7:
                 ctd[i].ATM_mutex = &atm7;
+                break;
+
             case 8:
                 ctd[i].ATM_mutex = &atm8;
+                break;
+
             case 9:
                 ctd[i].ATM_mutex = &atm9;
+                break;
+
             case 10:
                 ctd[i].ATM_mutex = &atm10;
+                break;
 
-                rc = pthread_create(&ct[i], NULL, request_payment, (void *) &ctd[i]);
-
-
-                if (rc) {
-                    cout << "Error:unable to create thread," << rc << endl;
-                    exit(-1);
-                }
         }
+        rc = pthread_create(&ct[i], NULL, request_payment, (void *) &ctd[i]);
 
+
+        if (rc) {
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+
+        }
+        i++;
     }
 }
 
 int main(int argc, char *argv[]) {
-    input_log = argv[1];
+    string input_log = argv[1];
     string output_log = input_log + "_log.txt";
-    ifstream file(input_log);
-    outfile.open(output_log);
-    file >> NUM_CUSTOMERS;
+
+    ifstream file;
+    string line;
+    file.open(input_log);
+    getline(file, line);
+    NUM_CUSTOMERS = stoi(line);
+
     pthread_t customer_t[NUM_CUSTOMERS];
     pthread_t atm_t[NUM_ATMS];
-    customer_thread_data ctd[NUM_CUSTOMERS];
+
     atm_thread_data atd[NUM_ATMS];
-    create_customers(file, customer_t, ctd);
+    customer_thread_data ctd[NUM_CUSTOMERS];
+
+    outfile.open(output_log);
     create_atms(atm_t, atd);
+    create_customers(file, customer_t, ctd);
     for (int i = 0; i < NUM_CUSTOMERS; i++)
         pthread_join(customer_t[i], NULL);
-    outfile<<"All payments are completed \n";
-    outfile<<"CableTV: "+ to_string(cableTv)+"\n";
-    outfile<<"Electricity: "+ to_string(electricity)+"\n";
-    outfile<<"Gas: "+ to_string(gas)+"\n";
-    outfile<<"Telecommunication: "+ to_string(telecommunications)+"\n";
-    outfile<<"Water: "+ to_string(water)+"\n";
+
+    outfile << "All payments are completed \n";
+    outfile << "CableTV: " + to_string(cableTv) + "\n";
+    outfile << "Electricity: " + to_string(electricity) + "\n";
+    outfile << "Gas: " + to_string(gas) + "\n";
+    outfile << "Telecommunication: " + to_string(telecommunications) + "\n";
+    outfile << "Water: " + to_string(water) + "\n";
     return 0;
 }
 
